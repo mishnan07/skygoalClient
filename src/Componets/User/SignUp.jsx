ee6fc682f27877c72fbedb0bc6b90cebc8a60246
mishnan <mishnanc@gmail.com>
2023-12-07 19:04:36 +0530

    complete3

--- src/Componets/User/SignUp.jsx
+++ src/Componets/User/SignUp.jsx
@@ -133,9 +133,10 @@ const SignUp = ({edit,user,setState}) => {
     }
   };
 
+
   return (
     <div>
-        <ToastContainer />
+      <ToastContainer position="top-center" />
 
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
